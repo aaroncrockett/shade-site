f933971374253b4185521cfcd63a47f14a7f875a
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-02-19 17:07:29 -0800

    init client and booking file reads

--- src/routes/calendar/+page.server.ts
+++ src/routes/calendar/+page.server.ts
@@ -1,4 +1,8 @@
 import { dev } from '$app/environment';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
@@ -7,3 +11,50 @@ export const csr = dev;
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = false;
+
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+	// have node find the file in $lib/data/appointments/2025 (current year)
+	// then the dir for the current month (2), and the next two months (it might have to go to the next year obviously)
+	//
+	// it should use this data to determine which days are available for appointments
+	// availableDays will be each of the type of day in the list (all mondays, tuesdays, etc)
+	// unless it is included in nonAvailableDates
+	// then also determine which days have already been booked
+	// {
+	//     "availableDays": ["thu, fri", "sat", "sun"],
+	//     "nonAvailableDates": [""],
+	//     "bookedDates": [
+	//         {
+	//             "date": "17",
+	//             "type": "consult",
+	//             "start": "11",
+	//             "end": "12",
+	//             "contactId": ""
+	//         },
+	//         { "date": "18", "type": "tat", "start": "12", "end": "5", "contactId": "" },
+	//         {
+	//             "date": "19",
+	//             "type": "consult",
+	//             "start": "11",
+	//             "end": "12",
+	//             "contactId": ""
+	//         }
+	//     ]
+	// }
+
+	const currentYear = new Date().getFullYear();
+	console.log(currentYear);
+
+	const filePath = path.resolve('src/lib/data/appointments/2025/2.json');
+
+	const fileContents = fs.readFileSync(filePath, 'utf-8');
+	console.log(fileContents);
+	return {
+		post: {
+			title: `Title for `,
+			content: `Content for `
+		}
+	};
+};
--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -1,8 +1,17 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
 <svelte:head>
 	<title>Calendar</title>
 	<meta name="description" content="Calendar of available time slots." />
 </svelte:head>
 
-<div class="text-column">
+<div>
 	<h1>Available time slots.</h1>
 </div>
+
+<h1>{data.post.title}</h1>
+<div>{@html data.post.content}</div>
