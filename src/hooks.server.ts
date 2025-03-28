defd948653cdea6dcd80e2890e37df5d725558f3
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-03-28 11:58:08 -0700

    redirect loc change

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -68,8 +68,7 @@ const authGuard: Handle = async ({ event, resolve }) => {
 	event.locals.user = user;
 
 	if (!event.locals.session && event.url.pathname.startsWith('/admin')) {
-		console.log('/auth');
-		redirect(303, '/auth');
+		redirect(303, '/');
 	}
 
 	if (event.locals.session && event.url.pathname === '/auth') {
