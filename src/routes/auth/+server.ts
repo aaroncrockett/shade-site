8dd06c40135b4717df0b6cfee93071535884c04b
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-03-28 12:08:08 -0700

    change auth logic

---
+++ src/routes/auth/+server.ts
@@ -0,0 +1,25 @@
+import { json, redirect } from '@sveltejs/kit';
+import { PUBLIC_APP_DOMAIN } from '$env/static/public';
+import { ADMIN_SEG } from '$lib/config';
+import type { RequestHandler } from './$types';
+
+export const GET: RequestHandler = async ({ locals: { supabase } }) => {
+	// could have multi auths in the future, but not planning to currently
+	const { data, error } = await supabase.auth.signInWithOAuth({
+		provider: 'google',
+		options: {
+			redirectTo: `${PUBLIC_APP_DOMAIN}${ADMIN_SEG}`
+		}
+	});
+
+	if (error) {
+		return json({ error: error.message }, { status: 400 });
+	}
+
+	if (data.url) {
+		// This will cause a real browser redirect (like in your load function)
+		throw redirect(303, data.url);
+	}
+
+	return json({ error: 'No URL returned from Supabase.' }, { status: 500 });
+};
