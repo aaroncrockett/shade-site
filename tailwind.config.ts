32c767f5bf23c4773547163caea59f5f8ff77c23
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-05 13:39:47 -0700

    font and style additions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,18 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-	content: ['./src/**/*.{html,js,svelte,ts}']
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				futura: ['futura-pt', 'sans-serif']
+			},
+			fontWeight: {
+				book: '400',
+				bold: '700'
+			}
+		}
+	}
 };
 
 export default config;
