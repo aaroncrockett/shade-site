87f90d8a9f17104524263b610068424fa405d126
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-08 19:09:15 -0700

    refactor for simplicty and use real tattoo img

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,25 +1,20 @@
 <script lang="ts">
 	import { LightboxGallery, GalleryThumbnail, GalleryImage } from 'svelte-lightbox';
 
-	import imgEx from '$lib/images/tattoos/ex/img-ex.png?enhanced';
-	import imgEx1 from '$lib/images/tattoos/ex/img-ex-1.png?enhanced';
-	import imgEx2 from '$lib/images/tattoos/ex/img-ex-2.png?enhanced';
-	import imgExT from '$lib/images/tattoos/ex/img-ex-t.png?enhanced';
-	import imgExT1 from '$lib/images/tattoos/ex/img-ex-t-1.png?enhanced';
-	import imgExT2 from '$lib/images/tattoos/ex/img-ex-t-2.png?enhanced';
+	import imgLg1 from '$lib/images/tattoos/snake.png?enhanced';
+	import imgTn1 from '$lib/images/tattoos/snake-sq.png?enhanced';
 
-	const thumbnails = [
-		{ id: 0, src: imgExT, alt: 'Tattoo 1 thumbnail', title: 'Tattoo 1' },
-		{ id: 1, src: imgExT1, alt: 'Tattoo 2 thumbnail', title: 'Tattoo 2' },
-		{ id: 2, src: imgExT2, alt: 'Tattoo 3 thumbnail', title: 'Tattoo 3' }
+	const images = [
+		{
+			id: 0,
+			thumbnail: imgTn1,
+			full: imgLg1,
+			title: 'Snake',
+			description: 'Black and grey, stipple shaded.'
+		}
 	];
 
-	const fullImages = [
-		{ id: 0, src: imgEx, title: 'Tattoo 1', description: 'Description for Tattoo 1' },
-		{ id: 1, src: imgEx1, title: 'Tattoo 2', description: 'Description for Tattoo 2' },
-		{ id: 2, src: imgEx2, title: 'Tattoo 3', description: 'Description for Tattoo 3' }
-	];
-
+	// Custom lightbox settings
 	const custom = {
 		lightboxContentProps: {
 			style: 'display: flex; align-items: center; justify-content: center;'
@@ -45,21 +40,22 @@
 <LightboxGallery enableClickToClose={true} customization={custom}>
 	<svelte:fragment slot="thumbnail">
 		<div class="s2s-custom-lb flex flex-wrap gap-4">
-			{#each thumbnails as { id, src, alt }}
+			{#each images as { id, thumbnail, title, description }}
 				<GalleryThumbnail {id}>
 					<enhanced:img
 						class="h-[150px] w-[150px] cursor-pointer rounded object-cover md:h-[250px] md:w-[250px] lg:h-[300px] lg:w-[300px]"
-						{src}
-						{alt}
+						src={thumbnail}
+						alt={`${title} - ${description}`}
+						{title}
 					/>
 				</GalleryThumbnail>
 			{/each}
 		</div>
 	</svelte:fragment>
 
-	{#each fullImages as { id, src, title, description }}
+	{#each images as { id, full, title, description }}
 		<GalleryImage {id} {title} {description}>
-			<enhanced:img {src} alt={title} />
+			<enhanced:img src={full} alt={title} />
 		</GalleryImage>
 	{/each}
 </LightboxGallery>
