4e3a9db5124ea4809ce9cff87fd46c8f8b21538d
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-08 20:46:07 -0700

    layout updates

--- src/lib/components/site-ui/page-main/index.svelte
+++ src/lib/components/site-ui/page-main/index.svelte
@@ -2,19 +2,35 @@
 	import snakeHero from '$lib/images/tattoos/snake.png?enhanced';
 </script>
 
-<section class="flex flex-col gap-4 py-4 sm:flex-row">
-	<div class="w-full sm:w-1/2">
-		<h1 class="bold text-3xl">New Spring Studio, Styles & Social Media!</h1>
+<section class="flex flex-col gap-0 py-0 sm:flex-row sm:gap-4 sm:py-4">
+	<div class="order-2 w-full sm:order-1 sm:w-1/2">
+		<h1 class="hero-hl text-100">
+			New <span class="!text-neutral-400"> Studio</span><span class="text-neutral-300"> ,</span>
+			<span class="text-neutral-400"> Styles</span> <span class="text-neutral-300"> &</span>
+			<span class="text-neutral-400"> Social Media</span>
+		</h1>
 		<section>
 			<h3>current location</h3>
 			<p>1234 example</p>
 		</section>
 	</div>
-	<div class="w-full sm:w-1/2">
+	<div class="order-1 w-full sm:order-2 sm:w-1/2">
 		<enhanced:img
 			src={snakeHero}
-			class="max-h-[250px] w-auto sm:max-h-[450px] lg:max-h-[600px]"
+			class="hero-img mx-auto max-h-[350px] w-auto sm:max-h-[450px] lg:max-h-[600px]"
 			alt="Black and gray stippled snake tattoo"
 		/>
 	</div>
 </section>
+
+<style>
+	.hero-img {
+		clip-path: inset(10% 0);
+	}
+
+	@media (min-width: 640px) {
+		.hero-img {
+			clip-path: inset(0);
+		}
+	}
+</style>
