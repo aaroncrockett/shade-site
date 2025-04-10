ac4a358c9cf5f68ca1f1392eb1d9918ae19441c2
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-10 10:20:15 -0700

    footer additions

--- src/lib/components/site-ui/footer/main-items.svelte
+++ src/lib/components/site-ui/footer/main-items.svelte
@@ -1,1 +1,4 @@
-<p>INSTAGRAM: @shade-to-shade-art</p>
+<div class="flex justify-between gap-4">
+	<p>INSTAGRAM: @shade-to-shade-art</p>
+	<p>Site created with SvelteKit and Dev'd by ShadeToShade.</p>
+</div>
