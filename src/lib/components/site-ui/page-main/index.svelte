8157ebc828aeb10acbca9694fcfe107bba5a9b60
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-10 10:32:22 -0700

    headline style and text update

--- src/lib/components/site-ui/page-main/index.svelte
+++ src/lib/components/site-ui/page-main/index.svelte
@@ -5,14 +5,18 @@
 <section class="flex flex-col gap-0 py-0 sm:flex-row sm:gap-4 sm:py-4">
 	<div class="order-2 flex w-full flex-col gap-6 sm:order-1 sm:w-1/2">
 		<h1 class="hero-hl text-100">
-			NEW <span class="!text-neutral-400"> Studio</span><span class="text-neutral-300">, </span>
-			<span class="text-neutral-400"> Styles</span><span class="text-neutral-300">, </span>
-			<br class="hidden sm:inline-block" />
+			Coming Soon! <br /> <span class="!text-neutral-400"> Tattooing</span>
+			<span class="text-neutral-400">
+				<span class="text-neutral-300"> @ </span>Black Dahlia Tattoo Co<span
+					class="text-neutral-300">.</span
+				> In May</span
+			>
+			<!-- <br class="hidden sm:inline-block" />
 			<span class="text-neutral-400"> Social Media</span>
 			<span class="text-neutral-300"> & <br class="hidden sm:inline-block" /></span>
 			<span class="text-neutral-400"
 				><span class="text-neutral-300">(</span>Web<span class="text-neutral-300">)</span> Site</span
-			>
+			> -->
 		</h1>
 		<section class="flex flex-col gap-4">
 			<p>I am currently creating this site, so bare with me while the features are sparse.</p>
