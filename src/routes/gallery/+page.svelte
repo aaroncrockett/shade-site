13649e3fc281ff267b3fcb218dd9ebbefb7f7b99
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-10 10:25:10 -0700

    update gallery w images and refine layout

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,30 +1,61 @@
 <script lang="ts">
 	import { LightboxGallery, GalleryThumbnail, GalleryImage } from 'svelte-lightbox';
 
-	import imgLg1 from '$lib/images/tattoos/snake.png?enhanced';
-	import imgTn1 from '$lib/images/tattoos/snake-sq.png?enhanced';
+	import imgSnakeLg from '$lib/images/tattoos/snake.png?enhanced';
+	import imgSnakeTn from '$lib/images/tattoos/snake-sq.png?enhanced';
+	import imgDocLg from '$lib/images/tattoos/p-doc.png?enhanced';
+	import imgDocTn from '$lib/images/tattoos/p-doc-sq.png?enhanced';
+	import imgDragonLg from '$lib/images/tattoos/dragon.png?enhanced';
+	import imgDragonTn from '$lib/images/tattoos/dragon-sq.png?enhanced';
+	import imgSkullLg from '$lib/images/tattoos/skull.png?enhanced';
+	import imgSkullTn from '$lib/images/tattoos/skull-sq.png?enhanced';
+	import imgFlowerLg from '$lib/images/tattoos/flower.png?enhanced';
+	import imgFlowerTn from '$lib/images/tattoos/flower-sq.png?enhanced';
+	import imgAcornLg from '$lib/images/tattoos/acorn.png?enhanced';
+	import imgAcornTn from '$lib/images/tattoos/acorn-sq.png?enhanced';
 
 	const images = [
 		{
 			id: 0,
-			thumbnail: imgTn1,
-			full: imgLg1,
+			thumbnail: imgSnakeTn,
+			full: imgSnakeLg,
 			title: 'Snake',
-			description: 'Black and grey, stipple shaded.'
+			description: 'Black and grey, stipple shaded snake.'
 		},
 		{
 			id: 1,
-			thumbnail: imgTn1,
-			full: imgLg1,
-			title: 'Snake',
-			description: 'Black and grey, stipple shaded.'
+			thumbnail: imgDocTn,
+			full: imgDocLg,
+			title: 'Plague Doctor',
+			description: 'Black work P Doc.'
 		},
 		{
 			id: 2,
-			thumbnail: imgTn1,
-			full: imgLg1,
-			title: 'Snake',
-			description: 'Black and grey, stipple shaded.'
+			thumbnail: imgDragonTn,
+			full: imgDragonLg,
+			title: 'Dragon',
+			description: 'Black and grey, stipple shaded dragon.'
+		},
+		{
+			id: 3,
+			thumbnail: imgSkullTn,
+			full: imgSkullLg,
+			title: 'Skull',
+			description: 'Blackwork skull.'
+		},
+		{
+			id: 4,
+			thumbnail: imgFlowerTn,
+			full: imgFlowerLg,
+			title: 'Flower',
+			description: 'Black and grey, stipple shaded flower.'
+		},
+		{
+			id: 5,
+			thumbnail: imgAcornTn,
+			full: imgAcornLg,
+			title: 'Acorn',
+			description: 'Outlined and stipple shaded acorn.'
 		}
 	];
 
@@ -52,13 +83,13 @@
 		<h1 class="text-center">TATTOOS</h1>
 	</div>
 
-	<div class="mx-auto flex w-full items-center justify-center gap-4">
+	<div class="mx-auto grid grid-cols-3 gap-2 sm:gap-3 lg:grid-cols-4">
 		<LightboxGallery enableClickToClose={true} customization={custom}>
 			<svelte:fragment slot="thumbnail">
 				{#each images as { id, thumbnail, title, description }}
 					<GalleryThumbnail {id}>
 						<enhanced:img
-							class="h-[120px] w-[120px] cursor-pointer rounded object-cover md:h-[200px] md:w-[200px] lg:h-[300px] lg:w-[300px]"
+							class="h-[80px] w-[80px] cursor-pointer rounded-sm object-cover sm:h-[120px] sm:w-[120px] md:h-[200px] md:w-[200px] lg:h-[300px] lg:w-[300px]"
 							src={thumbnail}
 							alt={`${title} - ${description}`}
 							{title}
