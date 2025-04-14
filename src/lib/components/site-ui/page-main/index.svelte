6198365b9b5df58ecbb7e3f11ef0d68604725f55
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-14 13:55:53 -0700

    wip - content

--- src/lib/components/site-ui/page-main/index.svelte
+++ src/lib/components/site-ui/page-main/index.svelte
@@ -8,9 +8,9 @@
 	class="flex flex-col gap-0 py-0 md:flex-row md:gap-4 md:py-4"
 	style={`background-image: url(${h1ImgBg}); background-repeat: repeat; background-position: center; background-size: 150px 150px;`}
 >
-	<div class="order-2 flex w-full flex-col gap-6 p-4 md:order-1 md:w-1/2">
+	<div class="order-2 flex w-full flex-col gap-6 p-4 leading-12 md:order-1 md:w-1/2">
 		<h1 class="hero-hl text-100">
-			I'm on a little break rn.
+			Booking for May rn!
 
 			<!-- <br class="hidden md:inline-block" />
 			<span class="text-neutral-400"> Social Media</span>
@@ -19,16 +19,17 @@
 				><span class="text-neutral-300">(</span>Web<span class="text-neutral-300">)</span> Site</span
 			> -->
 		</h1>
-		<h3 class="text-2xl leading-9 font-bold">
-			Feel Free To DM Me <em>@shade.to.shade.art</em> &nbsp; on BlueSky/Instagram, or email
-			shade.to.shade.art@proton.me -- to setup appointments for May.
+		<h2 class=" leading-11">
+			DM Me <em>@shade.to.shade.art</em> &nbsp; on BlueSky/Instagram, or email shade.to.shade.art@proton.me
+		</h2>
+		<h3 class="pt-2 text-2xl leading-9 font-bold">
+			I'm currently on break, until May, but I am taking consultations and booking Flash
+			appointments
 			<br />
 			<span class="inline-block pt-4"
-				>COMING IN MAY... I'll be tattooing @
+				>In May I'll be @
 
-				<span class=" text-neutral-400">
-					Black Dahlia Tattoo Co<span class="text-neutral-300">.</span>
-				</span> <br />
+				<span class="acumin text-neutral-100"> Black Dahlia Tattoo Co. </span> <br />
 			</span>
 			<span class="inline-block pt-4"><em>Booking system coming soon.</em></span>
 		</h3>
