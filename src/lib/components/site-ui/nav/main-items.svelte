8b3bd28850a997f9eb1f78015c6eb2a04679811c
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-13 19:15:57 -0700

    onclick event forwarding

--- src/lib/components/site-ui/nav/main-items.svelte
+++ src/lib/components/site-ui/nav/main-items.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-	import { page } from '$app/state';
+	export let onItemClick: () => void = () => {};
+	import { page } from '$app/stores';
 </script>
 
-<li aria-current={page.url.pathname === '/gallery' ? 'page' : undefined}>
-	<a class="text-white" href="/gallery">Gallery</a>
+<li aria-current={$page.url.pathname === '/gallery' ? 'page' : undefined}>
+	<a class="text-white" href="/gallery" on:click={onItemClick}>Gallery</a>
 </li>
-<li aria-current={page.url.pathname === '/bio' ? 'page' : undefined}>
-	<a class="text-white" href="/bio">Bio</a>
+<li aria-current={$page.url.pathname === '/bio' ? 'page' : undefined}>
+	<a class="text-white" href="/bio" on:click={onItemClick}>Bio</a>
 </li>
-<li aria-current={page.url.pathname === '/get-a-tattoo' ? 'page' : undefined}>
-	<a class="text-white" href="/get-a-tattoo">Get A Tattoo</a>
+<li aria-current={$page.url.pathname === '/get-a-tattoo' ? 'page' : undefined}>
+	<a class="text-white" href="/get-a-tattoo" on:click={onItemClick}>Get A Tattoo</a>
 </li>
