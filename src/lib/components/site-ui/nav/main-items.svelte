7e2b93c746e2a9cd8f0cd3b96785b31879b25777
Aaron Crockett <aaron.crockett.00@gmail.com>
2025-04-19 13:28:34 -0700

    wip

--- src/lib/components/site-ui/nav/main-items.svelte
+++ src/lib/components/site-ui/nav/main-items.svelte
@@ -4,21 +4,30 @@
 </script>
 
 <li
-	class="inline-block list-none md:hidden"
+	class="main-nav-li list-none md:hidden"
 	aria-current={page.url.pathname === '/' ? 'page' : undefined}
 >
 	<a class="text-white" href="/" onclick={onItemClick}>Home</a>
 </li>
 
-<li class="list-none" aria-current={page.url.pathname === '/tattoos' ? 'page' : undefined}>
+<li
+	class="main-nav-li list-none"
+	aria-current={page.url.pathname === '/tattoos' ? 'page' : undefined}
+>
 	<a class="text-white" href="/tatoos" onclick={onItemClick}>Tat Gallery</a>
 </li>
-<li class="list-none" aria-current={page.url.pathname === '/flash' ? 'page' : undefined}>
+<li
+	class="main-nav-li list-none"
+	aria-current={page.url.pathname === '/flash' ? 'page' : undefined}
+>
 	<a class="text-white" href="/flash" onclick={onItemClick}>Flash Gallery</a>
 </li>
-<li class="list-none" aria-current={page.url.pathname === '/bio' ? 'page' : undefined}>
+<li class="main-nav-li list-none" aria-current={page.url.pathname === '/bio' ? 'page' : undefined}>
 	<a class="text-white" href="/bio" onclick={onItemClick}>Bio</a>
 </li>
-<li class="list-none" aria-current={page.url.pathname === '/get-a-tattoo' ? 'page' : undefined}>
+<li
+	class="main-nav-li list-none"
+	aria-current={page.url.pathname === '/get-a-tattoo' ? 'page' : undefined}
+>
 	<a class="text-white" href="/get-a-tattoo" onclick={onItemClick}>Get A Tat</a>
 </li>
--- src/routes/get-a-tattoo/+page.svelte
+++ src/routes/get-a-tattoo/+page.svelte
@@ -14,24 +14,17 @@
 		<h1 class="text-center">Steps for getting a tattoo.</h1>
 	</BgImgWrapper>
 
-	<div class="flex flex-col gap-4 md:flex-row md:gap-6">
+	<div class="order-1 flex flex-col gap-4 lg:flex-row lg:gap-6">
 		<div>
 			<enhanced:img
 				src={s2sImg}
 				alt="Heart graphic."
-				class="mx-auto h-auto w-full max-w-[250px] rounded-sm md:w-[400px]"
+				class="mx-auto h-auto w-full max-w-[250px] rounded-sm lg:w-[400px]"
 			/>
 		</div>
-		<div class="flex flex-col gap-4 rounded-sm bg-neutral-700 p-4">
-			<h2>Instructions for getting a tattoo.</h2>
 
-			<div class="pl-4">
-				<p>
-					<a href="">Flash</a>.
-				</p>
-				<p><a href="">Prep instructions</a>.</p>
-				<p><a href="">Aftercare instructions</a>.</p>
-			</div>
+		<div class="order-3 flex flex-col gap-4 rounded-sm bg-neutral-700 p-4 lg:order-2">
+			<h2>Instructions for getting a tattoo.</h2>
 
 			<p>
 				If interested in getting a tattoo, or if you have any questions, DM on Instagram or send me
@@ -46,7 +39,7 @@
 			<ul class="list-disc pl-4">
 				<li>Flash: $25 deposit, to book.</li>
 				<li>
-					Designs you supply: $25 deposit, to book. Some designs I may suggest a free in-person
+					Supplied designs: $25 deposit, to book. Some designs I may suggest a free in-person
 					placement session to ensure the design flows correctly.
 				</li>
 				<li>
@@ -86,6 +79,18 @@
 					</ol>
 				</div>
 			</div>
+		</div>
+
+		<div
+			class="order-2 flex flex-col gap-2 rounded-sm bg-neutral-800/80 p-4 lg:order-3 lg:min-w-[170px] xl:min-w-[220px]"
+		>
+			<h4 class="mb-2 font-bold">Helpful Links</h4>
+
+			<p>
+				<a href="/flash">Flash.</a>
+			</p>
+			<!-- <p><a href="">Prep instructions.</a></p>
+			<p><a href="">Aftercare instructions.</a></p> -->
 		</div>
 	</div>
 </section>
